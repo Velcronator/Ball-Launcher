d0f48b2f9cdc5fa2400dac497a32f97c4d79624d
velcronator <valacos7@gmail.com>
2023-04-20 00:30:09 +1000

    World to Screen Space:

--- Assets/Scripts/BallHandler.cs
+++ Assets/Scripts/BallHandler.cs
@@ -2,6 +2,13 @@ using UnityEngine;
 using UnityEngine.InputSystem;
 public class BallHandler : MonoBehaviour
 {
+    private Camera m_Camera;
+
+    private void Start()
+    {
+        m_Camera = Camera.main;
+    }
+
     Vector2 _touchPosition;
     // Update is called once per frame
     void Update()
@@ -9,6 +16,8 @@ public class BallHandler : MonoBehaviour
         if (!Touchscreen.current.primaryTouch.press.isPressed) { return; }
         _touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
-        Debug.Log(_touchPosition.ToString());
+        Vector3 worldPosition = m_Camera.ScreenToWorldPoint(_touchPosition);
+
+        Debug.Log(worldPosition);
     }
 }
